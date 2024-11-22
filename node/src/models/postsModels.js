a81efe93edd57f611959a1444d37a6ae408ed71a
ATCO Dias <diaslasd@gmail.com>
2024-11-22 18:12:24 -0300

    ✨ feat: Nova funcionalidade implementada

--- node/src/models/postsModels.js
+++
@@ -1,9 +0,0 @@
-import conectarAoBanco from "../config/dbConfig.js";
-
-const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
-
-export async function getTodosPosts(){
-    const db = conexao.db("alura-imersao");
-    const colecao = db.collection("posts");
-    return colecao.find().toArray();
-}
--- node/src/routes/postsRoutes.js
+++ node/src/routes/postsRoutes.js
@@ -1,10 +1,23 @@
-import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import express from "express"; // Importa o framework Express para criar a aplicação web
+import multer from "multer"; // Importa o Multer para lidar com uploads de arquivos
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js"; // Importa as funções controladoras para lidar com a lógica dos posts
 
+// Cria uma instância do middleware Multer
+const upload = multer({ dest: "./uploads" });
+
+// Define as rotas usando o objeto Express app
 const routes = (app) => {
-    app.use(express.json());
-    
-    app.get("/posts", listarPosts);
-}
+  // Permite que o servidor interprete corpos de requisições no formato JSON
+  app.use(express.json());
+
+  // Rota para recuperar uma lista de todos os posts
+  app.get("/", listarPosts); // Chama a função controladora apropriada
+
+  // Rota para criar um novo post
+  app.post("/", postarNovoPost); // Chama a função controladora para criação de posts
+
+  // Rota para upload de imagens (assumindo uma única imagem chamada "imagem")
+  app.post("/upload", upload.single("imagem"), uploadImagem); // Chama a função controladora para processamento da imagem
+};
 
 export default routes;
